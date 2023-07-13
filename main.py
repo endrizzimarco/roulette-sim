473db2a7a31260aa80198c6b265c1826128eb86b
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-07-13 21:52:22 +0100

    feat: basic strategies

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-import random
+from strategies import Strategy
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def martingale(bankroll):
-    bet = 2.5
-    pockets = ["Red"] * 18 + ["Black"] * 18 + ["Green"] * 1
-    bankroll_history = []
-    while bankroll > 0:
-        if bet > bankroll:
-            bet = bankroll
-        roll = random.choice(pockets)
-        if roll == "Red":
-            bankroll += bet
-            bet = 2.5
-        else:
-            bankroll -= bet
-            bet *=2
-        bankroll_history.append(bankroll)
-    return bankroll_history
-    
-martingale(bankroll=100)
+if __name__ == "__main__":
+  plays = 2**12 # 1024
+  reached_session_aim = 0
+  for i in range(plays):
+    strategy = Strategy(bankroll=50, bet=2.5, session_aim=1000)
+    results = strategy.execute("irfans")
+    if max(results) >= strategy.session_aim: 
+      reached_session_aim += 1
+  print(f"Win probability for a session: {reached_session_aim/plays * 100}%")
+    # print(f"Total plays: {len(results)}")
+    # print(f"Max bankroll: {max(results)}")
 
-
-for i in range(6):
-    plt.plot(martingale(bankroll=150), linewidth=2)
+# for i in range(6):
+#     plt.plot(Strategy(bankroll=50, bet=2.5, session_aim=1000).execute("irfans"), linewidth=2)
     
-plt.xlabel("Number of Games", fontsize=18, fontweight="bold")
-plt.ylabel("Bankroll", fontsize=18, fontweight="bold")
-plt.xticks(fontsize=16, fontweight="bold")
-plt.yticks(fontsize=16, fontweight="bold")
-plt.title("Bankroll Over Time", fontsize=22, fontweight="bold")
-plt.show()
+# plt.xlabel("Number of Games", fontsize=18, fontweight="bold")
+# plt.ylabel("Bankroll", fontsize=18, fontweight="bold")
+# plt.xticks(fontsize=16, fontweight="bold")
+# plt.yticks(fontsize=16, fontweight="bold")
+# plt.title("Bankroll Over Time", fontsize=22, fontweight="bold")
+# plt.show()
+
--- strategies.py
+++ strategies.py
@@ -1,29 +1,75 @@
 import random
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-def always_red():  
-    total_cash = 150
-    pockets = ["Red"] * 18 + ["Black"] * 18 + ["Green"] * 1
-    history = []
-    while total_cash > 0:
-        roll = random.choice(pockets)
+def get_third(n): 
+    if n > 1 and n <= 12: return 1
+    if n > 12 and n <= 24: return 2
+    if n > 24 and n <= 36: return 3
+
+
+class Strategy: 
+    pockets = ["Red"] * 18 + ["Black"] * 18 + ["Green"] * 1  # European Roulette
+    last_pocket = 0
+
+    bankroll = 0 
+    bet = 0
+    inital_bet = 0
+    session_aim = 0
+    last_pocket = 1
+
+    def __init__(self, bankroll=100, bet=2.5, session_aim=250, american=False):
+        self.bankroll = bankroll
+        self.bet = bet
+        self.inital_bet = bet
+        self.session_aim = session_aim
+        if american: self.pockets += ["Green"]
+
+    def execute(self, strategy="always_red"):
+        bankroll_history = []
+        while self.bankroll > 0 and self.bankroll < self.session_aim:
+            self.strategies[strategy](self) # run strategy
+            bankroll_history.append(self.bankroll)
+        return bankroll_history
+
+
+    def always_red(self):  
+        roll = random.choice(self.pockets)
         if roll == "Red":
-            total_cash += 10
+            self.bankroll += self.bet
         else:
-            total_cash -= 10
-        history.append(total_cash)
-    print(len(history))
-    return history
-    
+            self.bankroll -= self.bet
 
-sns.set(rc={'figure.figsize':(11.7,8.27)})
-for i in range(4):
-    plt.plot(always_red(), linewidth=2)
-    
-plt.xlabel("Number of Games", fontsize=18, fontweight="bold")
-plt.ylabel("Bankroll", fontsize=18, fontweight="bold")
-plt.xticks(fontsize=16, fontweight="bold")
-plt.yticks(fontsize=16, fontweight="bold")
-plt.title("Bankroll Over Time", fontsize=22, fontweight="bold")
-plt.show()
+    def martingale(self):
+        roll = random.choice(self.pockets)
+        if roll == "Red":
+            self.bankroll += self.bet
+            self.bet = self.inital_bet
+        else:
+            self.bankroll -= self.bet
+            self.bet *= 2
+
+    def irfans(self):
+        roll = random.randint(0, 36)
+        if roll == 0 or get_third(roll) == get_third(self.last_pocket): 
+            self.bankroll -= 2 * self.bet
+            # self.bet *= 4
+        else:
+            self.bankroll += self.bet
+            # self.bet = self.inital_bet
+
+        self.last_pocket = roll
+        
+    def irfans_with_martingale(self):
+        roll = random.randint(0, 36)
+        if roll == 0 or get_third(roll) == get_third(self.last_pocket): 
+            self.bankroll -= 2 * self.bet
+            # self.bet *= 4
+        else:
+            self.bankroll += self.bet
+            # self.bet = self.inital_bet
+
+        self.last_pocket = roll
+
+    strategies = {'always_red': always_red, 'martingale': martingale, 'irfans': irfans}
+
+
+
