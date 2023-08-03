87f96ca2984a73eeae9819533a719f72ca9f88d6
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-08-03 20:57:53 +0100

    feat: basic lambda code

---
+++ lambda_function.py
@@ -0,0 +1,37 @@
+import json
+from simulate import simulate, optimise
+
+def lambda_handler(event, _context):
+    params = {
+    "sessions": 2**14, # 16384
+    "strat": "optimal_guetting",
+    "data": {
+        "bankroll": 50,
+        "bet": 2.5,
+        "profit_goal": 100,
+        "min_rounds": 0,
+        "max_rounds": 0,
+        "baccarat": False
+        },
+    # optimise params
+    "optimise_bet": False,
+    "win_chance": None
+    }
+
+    # Update default_params with the provided event data
+    for key, value in event.items():
+        if key == "data":
+            params[key].update(value)
+        else:
+            params[key] = value
+
+    if event.get("optimise"):
+        results = optimise(params)
+    else:
+        results = simulate(params)["success_rate"]
+
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(results)
+    }
