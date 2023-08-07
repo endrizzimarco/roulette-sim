bbecabbafd31a539e3a5ca99f36f4a139fe101c2
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-08-07 18:56:24 +0100

    fix: cli

--- cli.py
+++ cli.py
@@ -1,7 +1,7 @@
 from InquirerPy import prompt
 from InquirerPy.enum import INQUIRERPY_POINTER_SEQUENCE
 from InquirerPy.validator import EmptyInputValidator
-from strategies import Strategy
+from simulate import CasinoSession
 
 # TODO: support baccarat and blackjack odds
 questions = [
@@ -26,7 +26,7 @@ questions = [
     {
         "message": "Select the betting strategy:",
         "type": "list",
-        "choices": ["all"] + list(Strategy().strategies.keys()),
+        "choices": ["all"] + list(CasinoSession().strategies.keys()),
         "pointer": INQUIRERPY_POINTER_SEQUENCE,
     },
 ]
@@ -40,10 +40,10 @@ result = prompt(
 
 
 def simulate(result, strat):
-  simulations = 2**14 # 16384
+  simulations = 2**20 # 16384
   reached_session_aim = 0
   for _ in range(simulations):
-    strategy = Strategy(bankroll=float(result[0]), bet=float(result[1]), profit_goal=float(result[2]))
+    strategy = CasinoSession(bankroll=float(result[0]), bet_unit=float(result[1]), profit_goal=float(result[2]))
     results = strategy.execute(strat)
     if max(results["bankroll"]) >= strategy.profit_goal: 
       reached_session_aim += 1
@@ -51,7 +51,7 @@ def simulate(result, strat):
 
 
 if result[3] == "all":
-  for strat in Strategy().strategies.keys():
+  for strat in CasinoSession().strategies.keys():
     print(f'{strat}: {simulate(result, strat)}%')
 else: 
   chance = simulate(result, result[3])
